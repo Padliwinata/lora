6ef6b3b5d89e225432183b0b895e52b1e63f14fe
Rama Padliwinata <rpadliwinata@gmail.com>
2021-09-17 17:03:15 +0700

    updated

--- serial_reader.py
+++ serial_reader.py
@@ -52,7 +52,7 @@ def write_data(sec: int):
         # if response.status_code == 200:
         print(records)
         os.remove('data.txt')
-        print("end")
+        # print("end")
 
 
 if __name__ == "__main__":
