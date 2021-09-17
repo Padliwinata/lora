e61acecf39ce1855f50240a7fcaa21877437eb9f
Rama Padliwinata <rpadliwinata@gmail.com>
2021-09-17 13:58:49 +0700

    add writefile

--- serial_reader.py
+++ serial_reader.py
@@ -1,7 +1,9 @@
+import re
+
 import serial
 from serial.serialutil import SerialException
 
-
+regex = re.compile(r"((\d) VD(\d+\.\d)\s+A-(\d+\.\d)\s+B(\d+\.\d))")
 ser = None
 try:
     ser = serial.Serial('/dev/ttyUSB0')
@@ -9,11 +11,15 @@ try:
 except SerialException as e:
     print(e)
 
-while True:
-    try:
-        ser_bytes = ser.readline()
-        decoded_bytes = str(ser_bytes[0:len(ser_bytes)-2].decode('utf-8'))
-        print(decoded_bytes)
-    except KeyboardInterrupt:
-        print("Keyboard Interrupt")
-        break
+with open('dataFile.txt', 'a') as file:
+    while True:
+        try:
+            value = ser.readline().decode('UTF-8')
+            file.write(value)
+            print(value)
+            # ser_bytes = ser.readline()
+            # decoded_bytes = str(ser_bytes[0:len(ser_bytes)-2].decode('latin-1'))
+            # print(decoded_bytes)
+        except KeyboardInterrupt:
+            print("Keyboard Interrupt")
+            break
