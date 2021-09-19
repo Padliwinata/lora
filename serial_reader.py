2be89e448008c1e2c32fe1d851c53c6575a65840
Padliwinata <rpadliwinata@gmail.com>
2021-09-19 11:32:45 +0700

    fixed

--- serial_reader.py
+++ serial_reader.py
@@ -1,14 +1,15 @@
-import re
-import time
-import os
-import requests
 import datetime
+import os
+import re
+import requests
+import time
 
 import serial
 from serial.serialutil import SerialException
 
-regex = re.compile(r"((\d) VD(\d+\.\d)\s+A-(\d+\.\d)\s+B(\d+\.\d))")
-ser = None
+
+regex = re.compile(r"((\d)\sV(\d+\.\d)\s+A(\d+\.\d)\s+B(\d+\.\d))")
+
 try:
     ser = serial.Serial('/dev/ttyUSB0')
     ser.flushInput()
@@ -16,49 +17,47 @@ except SerialException as e:
     print(e)
 
 
-def write_data(sec: int):
-    timeout = time.time() + sec
-    file = open('data.txt', 'a')
+def read_and_send(sec: int):
     while True:
-        if time.time() > timeout:
-            break
-        value = ser.readline().decode('UTF-8')
-        value.rstrip()
-        file.write(value)
-    file.close()
-
-
-def send_data():
-    while True:
-        datas = []
-        with open('data.txt', 'r') as file:
+        timeout = time.time() + sec
+        file = open('gateway_data.txt', 'a')
+        while True:
+            if time.time() > timeout:
+                break
+            value = ser.readline().decode('utf-8')
+            file.write(value)
+        file.close()
+        with open('gateway_data.txt', 'r') as file:
             for line in file:
-                if line:
-                    if line[0].isdigit():
-                        line = line.replace("WAIT...", "")
-                        res = regex.findall(line)
-                        now = datetime.datetime.now()
-                        if len(res) != 0:
-                            data = {
-                                'params': 'insert-data',
-                                'records': {
-                                    'date': now.strftime("%Y-%m-%d"),
-                                    'time': now.strftime("%H:%M"),
-                                    'voltage': res[0][4],
+                if line[0].isdigit():
+                    line = line.rstrip()
+                    res = regex.findall(line)
+                    if len(res) != 0:
+                        print(f"Device: \t{res[0][1]}")
+                        print(f"Voltase:\t{res[0][2]}")
+                        print(f"Arus:\t\t{res[0][3]}")
+                        print(f"Baterai:\t{res[0][4]}")
+                        print()
+                        data = {
+                            'params': 'insert-data',
+                            'records': [
+                                {
+                                    'date': datetime.datetime.now().strftime("%Y-%m-%d"),
+                                    'time': datetime.datetime.now().strftime("%H:%M"),
+                                    'voltage': res[0][2],
                                     'current': res[0][3],
-                                    'battery': res[0][2]
-                                },
-                                'device': res[0][1]
-                            }
-                            datas.append()
-                            print(data)
-
-        # response = requests.post('http://tomas.pgn-solution.co.id:14000/api/public/device/smart-tb', json=data)
-        # if response.status_code == 200:
-        # print(data)
-        os.remove('data.txt')
+                                    'battery': round(float(res[0][4]), 0)
+                                }
+                            ],
+                            'device': str(res[0][1])
+                        }
+                        requests.post('http://tomas.pgn-solution.co.id:14000/api/public/device/smart-tb', json=data)
+        os.remove('gateway_data.txt')
+        print('sent')
         print()
 
 
-if __name__ == "__main__":
-    write_data(10)
+if __name__ == '__main__':
+    read_and_send(30)
+
+
